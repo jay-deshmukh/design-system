788674d0cdf2b6e7db6c5fddc53273da9b168615
Jay Deshmukh <jaydeshmukh8551@gmail.com>
2020-06-13 19:45:10 +0530

    build

---
+++ storybook-static/precache-manifest.0da71c1bfc007ed013e0ef346f715f88.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95c9768abef5dea6c9a603216867da07",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.7008a89a538d9e12deee.bundle.js"
+  },
+  {
+    "url": "runtime~main.7008a89a538d9e12deee.bundle.js"
+  },
+  {
+    "revision": "c9f6854bd7bd855922d1a520dba7c864",
+    "url": "static/media/signup.c9f6854b.svg"
+  },
+  {
+    "url": "vendors~main.7008a89a538d9e12deee.bundle.js"
+  },
+  {
+    "url": "vendors~main.7008a89a538d9e12deee.bundle.js.LICENSE.txt"
+  }
+]);
